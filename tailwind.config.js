0aa56a1f72e1718b93c70af582e71b6d260e2d47
vpinchaudhary <67816970+vpinchaudhary@users.noreply.github.com>
2021-06-17 06:47:30 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+const defaultColors = require('tailwindcss/colors')
+
+module.exports = {
+  mode: 'jit',
+  purge: ['./index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      primary: {
+        50: '#F1F8F5',
+        100: '#D4E9E2',
+        200: '#008248',
+        300: '#00653E',
+        400: '#1E3932',
+      },
+      secondary: {
+        100: '#F3F1E7',
+        200: '#CBA258',
+      },
+      white: defaultColors.white,
+      gray: defaultColors.gray,
+      black: defaultColors.black,
+    },
+
+    extend: {
+      height: {
+        18: '4.5rem',
+      },
+      backgroundImage: (theme) => ({
+        'mobile-star':
+          "url('https://www.starbucks.com/weblx/images/rewards/hero/hero-mobile_2021.jpg')",
+      }),
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
